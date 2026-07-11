ea64989bce9f21f8035d3a0f5090dada7528fbc3
Georg Lerm <glerm@gltech.co.za>
2026-07-11 16:48:22 +0200

    updated delay

--- esp32-blink/src/main.cpp
+++ esp32-blink/src/main.cpp
@@ -4,7 +4,7 @@
 #define RGB_LED_PIN 48
 #define NUM_RGB_LEDS 1
 #define STEP_DELAY_MS 300
-#define LED_BRIGHTNESS 10
+#define LED_BRIGHTNESS 20
 
 Adafruit_NeoPixel rgbLed(NUM_RGB_LEDS, RGB_LED_PIN, NEO_GRB + NEO_KHZ800);
 
